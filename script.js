c252d1c4691b29d1ab9376492219c1c116144e70
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-01-30 13:01:05 -0800

    Add menu function to script.js for UI buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = menu
+
+function menu() {
+  document.getElementById("output").innerHTML=`<span style="display: flex; justify-content: center;">
+  <button onclick="cryptanalysis()" class="main-button">Cryptanalysis</button>
+  <button onclick="utilities()" class="main-button">Utilities</button></br>
+  <button onclick="games()" class="main-button">Games</button>
+  <button onclick="spoof()" class="main-button">Spoof</button></br>
+  <button onclick="miscellaneous()" class="main-button">Miscellaneous</button>
+  <button onclick="comments()" class="main-button">Comments</button>
+  </span>`;
+}
