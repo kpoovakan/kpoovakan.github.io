762027ff9e243655bad9573531ebb08f9c04282a
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-01-30 13:43:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = menu
 
 function menu() {
   document.getElementById("output").innerHTML=`<span style="width: 600px; display: block; margin: 13px auto; padding: 31px;">
-  <button onclick="cryptanalysis()" class="main-button" style="float: left;">Cryptanalysis</button>
-  <button onclick="utilities()" class="main-button" style="float: right;">Utilities</button></br></br><div class="main-clear"></div>
-  <button onclick="games()" class="main-button" style="float: left;">Games</button>
-  <button onclick="spoof()" class="main-button" style="float: right;">Spoof</button></br></br><div class="main-clear"></div>
-  <button onclick="miscellaneous()" class="main-button" style="float: left;">Miscellaneous</button>
-  <button onclick="comments()" class="main-button" style="float: right;">Comments</button>
+  <button onclick="cryptanalysis()" class="main-button" style="float: left; margin: 13px auto">Cryptanalysis</button>
+  <button onclick="utilities()" class="main-button" style="float: right; margin: 13px auto">Utilities</button></br></br><div class="main-clear"></div>
+  <button onclick="games()" class="main-button" style="float: left; margin: 13px auto">Games</button>
+  <button onclick="spoof()" class="main-button" style="float: right; margin: 13px auto">Spoof</button></br></br><div class="main-clear"></div>
+  <button onclick="miscellaneous()" class="main-button" style="float: left; margin: 13px auto">Miscellaneous</button>
+  <button onclick="comments()" class="main-button" style="float: right; margin: 13px auto">Comments</button>
   <div class="main-clear"></div></span>`;
 }
 
