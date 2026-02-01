5bc4e8770b7a835f4d92ae5f79472d74f2cd3a82
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-02-01 12:22:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,7 @@ function comments() {
 function cryptanalysis() {
   document.getElementById("output").innerHTML=`<span style="width:600px; display: block; margin: 13px auto; padding: 31px;">
   <button onclick="menu()" class="main-small-button">Back to menu</button></br>
+  <p><a href="/vigeneretableau">The Vigenère Tableau</a>: encipher and decipher messages using Vigenère's Tableau Cipher. Ciphertext, if written with a valid method, can be deciphered even if the key letter is unknown.</p>
+  <p><a href="/caesarcipher">Caesar Cipher</a>: encipher and decipher messages using the Caesar Cipher. Ciphertext can be converted to plaintext even if the key letter is unknown.</p>
   </span>`;
 }
