1db7856115ed8d00a178de282fec1adbd3039ae6
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-02-04 13:36:04 -0800

    Create 404.js

---
+++ 404.js
@@ -0,0 +1,25 @@
+function fork() {
+    const pie = await fetch(window.location.href.toLowerCase(), { method: "HEAD" }); {
+      if (pie.status !== 404) {
+      window.location.replace(window.location.href.toLowerCase());
+      }
+  }
+}
+
+function pi() {
+  const fileUrl = '/pumpkinpi/pi.txt';
+   fetch(fileUrl)
+    .then(response => {
+       if (!response.ok) throw new Error('File not found');
+       return response.text(); // Convert the data to plain text
+     })
+    .then(data => {
+       const trimmed = data.replace(/[\r\n]+$/, '');
+       document.getElementById('content-target').textContent = trimmed + '...';
+     })
+     .catch(error => {
+       console.error('Error:', error);document.getElementById('content-target').textContent = 'Failed to load file.';
+         });
+}
+
+window.onload = fork
