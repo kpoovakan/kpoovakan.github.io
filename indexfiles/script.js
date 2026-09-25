ede7443cc1e973d70884b642dec1c33c1ecd1e80
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-09-25 10:53:34 -0700

    Add noscript

---
+++ indexfiles/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const content = document.getElementById("content");
+window.addEventListener("load", screenTitle());
+
+function screenTitle() {
+    content.innerHTML = `<h1>kpoovakan</h1>`;
+}
